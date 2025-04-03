f2eccacd3eddd388b89fba66ca6a1ff6a5f5d6f8
Ivan Bokuyemskyy <ivan.bokuyemskyy@gmail.com>
2025-04-03 18:12:25 +0200

    feat: implemented packaging resources into a source file

---
+++ project/source/utils.cpp
@@ -0,0 +1,6 @@
+#include "utils.hpp"
+
+float Utils::distance(const std::pair<float, float> &a, const std::pair<float, float> &b) {
+    return std::sqrt(((a.first - b.first) * (a.first - b.first)) +
+                     ((a.second - b.second) * (a.second - b.second)));
+}
