45e7f7e8ea82d025c40d0284999fb735d15e73f2
Ivan Bokuyemskyy <ivan.bokuyemskyy@gmail.com>
2025-04-17 08:58:41 +0200

    refactor: embraced modern cpp practices in performance-important parts of the code

--- project/source/game.cpp
+++ project/source/game.cpp
@@ -1,16 +1,3 @@
 #include "game.hpp"
 
-#include <array>
-
 #include "types.hpp"
-
-void Game::holdPiece(int index) {
-    if (m_position.pieceAt(Square(index)) == Piece::NONE) {
-        m_selectedSquare = Square::NONE;
-    } else {
-        m_selectedSquare = Square(index);
-        m_holdingPiece = true;
-    }
-}
-
-void Game::releasePiece() { m_holdingPiece = false; }
