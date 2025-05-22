021676c495d5ffdc1d63a55947265f68c9075fa7
Ivan Bokuyemskyy <ivan.bokuyemskyy@gmail.com>
2025-05-22 23:48:24 +0200

    refactor: code readability refactor, perft improvement. i made everything work perfect but something broke and now movement system fails to work correctly -_-

--- project/include/game/position.hpp
+++ project/include/game/position.hpp
@@ -2,6 +2,7 @@
 #define POSITION_HPP
 
 #include <array>
+#include <cstddef>
 #include <list>
 #include <string>
 #include <vector>
@@ -11,37 +12,42 @@
 #include "piece.hpp"
 #include "square.hpp"
 
-constexpr auto DEFAULT_FEN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR";
+constexpr auto DEFAULT_FEN       = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR";
+const int      MAX_DEPTH         = 64;  // Hard cap for recursion
+const int      MAX_MOVES_PER_POS = 256;
 
 class Position {
    public:
-    Position(const std::string &fen = DEFAULT_FEN) { setFromFEN(fen); }
+    Position(const std::string &fen = DEFAULT_FEN) {
+        setFromFEN(fen);
+        updateCachedState();
+        m_moveBuffer.reserve(MAX_MOVES_PER_POS * MAX_DEPTH);
+    }
 
     void                      setFromFEN(const std::string &fen);
     [[nodiscard]] std::string toFEN() const;
 
-    void                setPiece(Square square, Piece p);
-    void                unsetPiece(Square square);
-    void                resetBoard();
+    void setPiece(Square square, Piece p);
+    void unsetPiece(Square square);
+    void resetBoard();
+
     [[nodiscard]] Piece pieceAt(Square square) const;
+
+    void updateCachedState();
+    void toggleTurn();
 
     [[nodiscard]] bool isLegal();
 
-    void generatePawnMoves(std::vector<Move> &moves, Square square) const;
-    void generateKnightMoves(std::vector<Move> &moves, Square square) const;
-    void generateBishopMoves(std::vector<Move> &moves, Square square) const;
-    void generateRookMoves(std::vector<Move> &moves, Square square) const;
-    void generateQueenMoves(std::vector<Move> &moves, Square square) const;
-    void generateKingMoves(std::vector<Move> &moves, Square square) const;
-
-    void generatePseudoLegalMoves(std::vector<Move> &moves);
-    int  perft(uint8_t depth, bool verbose = false);
+    size_t generatePseudoLegalMoves(std::vector<Move> &moves);
+    int    perft(uint8_t depth, bool verbose = false);
 
     void                doMove(Move move);
     void                undoMove();
     [[nodiscard]] Color turn() const { return m_turn; }
 
    private:
+    std::vector<Move> m_moveBuffer;
+
     std::array<Piece, Squares::NB>       m_board{};
     std::array<Bitboard, Colors::NB>     m_colorBB{BITBOARD_ZERO};
     std::array<Bitboard, PieceTypes::NB> m_pieceTypeBB{BITBOARD_ZERO};
@@ -52,6 +58,12 @@ class Position {
     Color    m_turn      = Colors::WHITE;
     Castling m_castling  = Castlings::ANY;
     uint8_t  m_halfmoves = 0;
+
+    Bitboard m_occupancy{};
+    Square   m_wking{};
+    Square   m_bking{};
+    Color    m_us{};
+    Color    m_them{};
 };
 
 #endif
