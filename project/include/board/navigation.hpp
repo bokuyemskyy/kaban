763fed6a8a2fb34404f06c962abec34ab29873f8
bokuyemskyy <i20808753@gmail.com>
2025-07-05 21:31:06 +0200

    feat: renamed 'navigation' to 'coordinate', implemented CRTP in coordinate

--- project/include/board/navigation.hpp
+++ project/include/board/navigation.hpp
@@ -1,7 +1,5 @@
-#ifndef SQUARE_HPP
-#define SQUARE_HPP
-
-#include <netinet/in.h>
+#ifndef NAVIGATION_HPP
+#define NAVIGATION_HPP
 
 #include <array>
 #include <cstdint>
@@ -11,10 +9,36 @@
 
 #include "piece.hpp"
 
-struct File {
-   private:
+template <typename Derived>
+struct BoardCoordinate {
+   protected:
     uint8_t m_val;
 
+   private:
+    constexpr BoardCoordinate(uint8_t val) : m_val(val) {}
+
+   public:
+    constexpr operator uint8_t() const { return m_val; }
+
+    [[nodiscard]] constexpr bool isValid() const { return m_val <= Derived::LAST; }
+
+    constexpr Derived& operator++() {
+        ++m_val;
+        return *this;
+    }
+    constexpr Derived operator++(int) {
+        Derived old = *this;
+        ++(*this);
+        return old;
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const Derived& obj) {
+        obj.print(os);
+        return os;
+    };
+    friend Derived;
+};
+struct File : BoardCoordinate<File> {
    public:
     // clang-format off
     enum : uint8_t { 
@@ -30,36 +54,20 @@ struct File {
     };
     // clang-format on
 
-    constexpr File(uint8_t val = NONE) : m_val(val) {};
+    constexpr File(uint8_t val = NONE) : BoardCoordinate(val) {}
 
-    constexpr operator uint8_t() const { return m_val; }
+    static constexpr uint8_t distance(File from, File to) { return from > to ? from - to : to - from; }
 
-    constexpr File& operator++() {
-        ++m_val;
-        return *this;
-    }
-    constexpr File operator++(int) {
-        File old = *this;
-        ++(*this);
-        return old;
-    }
-
-    static constexpr uint8_t distance(File from, File to) { return abs(from - to); }
-
-    friend std::ostream& operator<<(std::ostream& os, const File& file) {
+    void print(std::ostream& os) const {
         static constexpr std::string_view fileToChar = "ABCDEFGH";
-        if (file <= File::LAST) {
-            os << fileToChar[file];
+        if (isValid()) {
+            os << fileToChar[m_val];
         } else
             os << '?';
-        return os;
     }
 };
 
-struct Rank {
-   private:
-    uint8_t m_val;
-
+struct Rank : BoardCoordinate<Rank> {
    public:
     // clang-format off
     enum : uint8_t { 
@@ -75,21 +83,9 @@ struct Rank {
     };
     // clang-format on
 
-    constexpr Rank(uint8_t val = NONE) : m_val(val) {}
+    constexpr Rank(uint8_t val = NONE) : BoardCoordinate(val) {}
 
-    constexpr operator uint8_t() const { return m_val; }
-
-    constexpr Rank& operator++() {
-        ++m_val;
-        return *this;
-    }
-    constexpr Rank operator++(int) {
-        Rank old = *this;
-        ++(*this);
-        return old;
-    }
-
-    static constexpr uint8_t distance(Rank from, Rank to) { return abs(from - to); }
+    static constexpr uint8_t distance(Rank from, Rank to) { return from > to ? from - to : to - from; }
 
     [[nodiscard]] constexpr bool pawnStarting(Color color) const {
         return (color == Colors::WHITE && m_val == R2) || (color == Colors::BLACK && m_val == R7);
@@ -98,20 +94,16 @@ struct Rank {
         return (color == Colors::WHITE && m_val == R8) || (color == Colors::BLACK && m_val == R1);
     }
 
-    friend std::ostream& operator<<(std::ostream& os, const Rank& rank) {
+    void print(std::ostream& os) const {
         static constexpr std::string_view fileToChar = "12345678";
-        if (rank <= Rank::LAST) {
-            os << fileToChar[rank];
+        if (isValid()) {
+            os << fileToChar[m_val];
         } else
             os << '?';
-        return os;
     }
 };
 
-struct Square {
-   private:
-    uint8_t m_val;
-
+struct Square : BoardCoordinate<Square> {
    public:
     // clang-format off
     enum : uint8_t {
@@ -134,43 +126,25 @@ struct Square {
     };
     // clang-format on
 
-    constexpr Square(uint8_t val = NONE) : m_val(val) {}
-
-    constexpr operator uint8_t() const { return m_val; }
-
-    constexpr Square& operator++() {
-        ++m_val;
-        return *this;
-    }
-    constexpr Square operator++(int) {
-        Square old = *this;
-        ++(*this);
-        return old;
-    }
+    constexpr Square(uint8_t val = NONE) : BoardCoordinate(val) {}
 
     [[nodiscard]] constexpr File file() const { return m_val & File::MASK; }
     [[nodiscard]] constexpr Rank rank() const { return m_val >> File::SIZE; }
 
     [[nodiscard]] static constexpr Square create(File file, Rank rank) { return rank << File::SIZE | file; }
 
-    static uint8_t distance(Square from, Square to) {
-        static auto table = [] {
+    static constexpr uint8_t distance(Square from, Square to) {
+        static constexpr auto table = []() constexpr {
             std::array<std::array<uint8_t, NB>, NB> t{};
-            for (Square i = Square::FIRST; i <= Square::LAST; ++i) {
-                for (Square j = Square::FIRST; j <= Square::LAST; ++j) {
-                    t[i][j] = std::max(abs(i.file() - j.file()), abs(i.rank() - j.rank()));
+            for (uint8_t i = FIRST; i <= LAST; ++i) {
+                for (uint8_t j = FIRST; j <= LAST; ++j) {
+                    auto from = Square(i), to = Square(j);
+                    t[i][j] = std::max(File::distance(from.file(), to.file()), Rank::distance(from.rank(), to.rank()));
                 }
             }
             return t;
         }();
         return table[from][to];
-    }
-
-    [[nodiscard]] constexpr bool isValid() const { return m_val <= Square::LAST; }
-
-    friend std::ostream& operator<<(std::ostream& os, const Square& square) {
-        os << square.file() << square.rank();
-        return os;
     }
 };
 
