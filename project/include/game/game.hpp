b13a67b16293a9321c5acf4c93e40ec77e09527d
Ivan Bokuyemskyy <ivan.bokuyemskyy@gmail.com>
2025-08-02 07:35:20 +0200

    feat(game): added position wrapper functions to ensure encapsulation

--- project/include/game/game.hpp
+++ project/include/game/game.hpp
@@ -1,11 +1,22 @@
 #ifndef GAME_HPP
 #define GAME_HPP
 
+#include <string>
+
 #include "position.hpp"
 
 class Game {
    public:
-    [[nodiscard]] Position& getPosition() { return m_position; }
+    void start() { m_position.setFromFEN(); }
+    void reset() { m_position.setFromFEN("8/8/8/8/8/8/8/8"); }
+
+    void doMove(const std::string /*moveString*/) { m_position.doMove(Move()); }
+    void doMove(int /*from*/, int /*to*/) { m_position.doMove(Move()); }
+    // dummy wrapper for the position
+    // ensures encapsulation without game.getPosition().doMove()
+    // the same should be implemented with undo
+
+    // get setters for string
 
    private:
     Position                            m_position;
