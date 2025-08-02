fa42cb3c6c52b24bb8d4fb130b8632cd634965e4
bokuyemskyy <i20808753@gmail.com>
2025-08-02 21:12:00 +0200

    fix: both renderer and UCI can close the app now (async)

--- project/main.cpp
+++ project/main.cpp
@@ -11,9 +11,9 @@ int main() {
     renderer.start(800, 600, "Kaban", true, app_state);
     uci.start(app_state);
 
-    // while (!app_state.shouldQuit()) {
-    //     std::this_thread::sleep_for(std::chrono::milliseconds(100));
-    // }
+    while (!app_state.shouldQuit()) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
 
     renderer.stop();
     uci.stop();
