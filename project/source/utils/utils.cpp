e3fedf0196bf7c6190e44a09a56f77394d1ef8be
Ivan Bokuyemskyy <ivan.bokuyemskyy@gmail.com>
2025-08-02 20:23:49 +0200

    refactor(renderer): MVP

--- project/source/utils/utils.cpp
+++
@@ -1,5 +0,0 @@
-#include "utils.hpp"
-
-float eucledianDistance(const std::pair<float, float>& a, const std::pair<float, float>& b) {
-    return std::sqrt(((a.first - b.first) * (a.first - b.first)) + ((a.second - b.second) * (a.second - b.second)));
-}
--- project/tests/pieces.cpp
+++ project/tests/pieces.cpp
@@ -3,12 +3,12 @@
 #include "piece.hpp"
 
 TEST(Pieces, CreateGet) {
-    EXPECT_EQ(Piece(Color::BLACK, PieceType::ROOK), Piece::B_ROOK);
-    EXPECT_EQ(Piece(Color::WHITE, PieceType::PAWN), Piece::W_PAWN);
+    EXPECT_EQ(Piece(Color::BLACK, PieceType::ROOK).value(), Piece::B_ROOK);
+    EXPECT_EQ(Piece(Color::WHITE, PieceType::PAWN).value(), Piece::W_PAWN);
 
-    EXPECT_EQ(Piece(Color::WHITE, PieceType::PAWN).color(), Color::WHITE);
-    EXPECT_EQ(Piece(Color::BLACK, PieceType::PAWN).color(), Color::BLACK);
+    EXPECT_EQ(Piece(Color::WHITE, PieceType::PAWN).color().value(), Color::WHITE);
+    EXPECT_EQ(Piece(Color::BLACK, PieceType::PAWN).color().value(), Color::BLACK);
 
-    EXPECT_EQ(Piece(Color::WHITE, PieceType::KING).pieceType(), PieceType::KING);
-    EXPECT_EQ(Piece(Color::BLACK, PieceType::QUEEN).pieceType(), PieceType::QUEEN);
+    EXPECT_EQ(Piece(Color::WHITE, PieceType::KING).pieceType().value(), PieceType::KING);
+    EXPECT_EQ(Piece(Color::BLACK, PieceType::QUEEN).pieceType().value(), PieceType::QUEEN);
 }
