b25056bfbf1e667960262776cbcb706c476d0b7f
Ivan Bokuyemskyy <ivan.bokuyemskyy@gmail.com>
2025-08-06 23:06:21 +0200

    refactor: moves / state separation

--- app/moves/move.hpp
+++ app/moves/move.hpp
@@ -1,12 +1,7 @@
 #pragma once
 
-#include <cstdint>
-
-#include "castling.hpp"
-#include "en_passant.hpp"
 #include "navigation.hpp"
 #include "piece.hpp"
-
 struct Flag : Metadata<Flag> {
    public:
     enum : uint8_t {
@@ -88,33 +83,4 @@ struct Move {
     static constexpr int TO_SHIFT        = Square::size();
     static constexpr int FLAG_SHIFT      = 2 * Square::size();
     static constexpr int PROMOTION_SHIFT = (2 * Square::size()) + Flag::size();
-};
-
-struct State {
-   public:
-    constexpr State(Castling castling, EnPassant en_passant, uint8_t halfmoves)
-        : m_value(static_cast<uint32_t>(castling.value() & Castling::mask()) << CASTLING_SHIFT |
-                  static_cast<uint32_t>(en_passant.value() & EnPassant::mask()) << EN_PASSANT_SHIFT |
-                  static_cast<uint32_t>(halfmoves & HALFMOVES_MASK) << HALFMOVES_SHIFT) {}
-
-    [[nodiscard]] constexpr Castling castling() const { return (m_value >> CASTLING_SHIFT) & Castling::mask(); }
-
-    [[nodiscard]] constexpr EnPassant en_passant() const { return (m_value >> EN_PASSANT_SHIFT) & EnPassant::mask(); }
-
-    [[nodiscard]] constexpr uint8_t halfmoves() const { return (m_value >> HALFMOVES_SHIFT) & HALFMOVES_MASK; }
-
-    constexpr void setCaptured(Piece captured) { m_value |= ((captured.value() & Piece::mask()) << CAPTURED_SHIFT); }
-    [[nodiscard]] constexpr Piece captured() const { return (m_value >> CAPTURED_SHIFT) & Piece::mask(); }
-
-   private:
-    uint16_t m_value;
-
-    static constexpr int HALFMOVES_BITS = 6;
-
-    static constexpr int CASTLING_SHIFT   = 0;
-    static constexpr int EN_PASSANT_SHIFT = CASTLING_SHIFT + Castling::size();
-    static constexpr int HALFMOVES_SHIFT  = EN_PASSANT_SHIFT + EnPassant::size();
-    static constexpr int CAPTURED_SHIFT   = HALFMOVES_SHIFT + HALFMOVES_BITS;
-
-    static constexpr uint32_t HALFMOVES_MASK = 0b111111;
 };
