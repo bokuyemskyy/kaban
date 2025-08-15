78308eeb78a48e99fb2bc951f9a90466af4838f8
Ivan Bokuyemskyy <ivan.bokuyemskyy@gmail.com>
2025-08-15 20:13:35 +0200

    feat: strong, safetype CRTPs for values

--- app/core/metadata.hpp
+++
@@ -1,8 +0,0 @@
-#include <cstdint>
-template <typename Derived>
-struct Metadata {
-    [[nodiscard]] static constexpr uint8_t number() { return Derived::NB; }
-    [[nodiscard]] static constexpr uint8_t mask() { return Derived::MASK; }
-    [[nodiscard]] static constexpr uint8_t size() { return Derived::SIZE; }
-    friend Derived;
-};
