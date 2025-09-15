cbc59c7cb90fa0ff6671bd2498410bf434af0d93
Ivan Bokuyemskyy <ivan.bokuyemskyy@gmail.com>
2025-09-15 16:28:34 +0200

    feat: engine

--- app/engine/engine.hpp
+++ app/engine/engine.hpp
@@ -1,31 +1,62 @@
 #pragma once
 
+#include <algorithm>
 #include <ranges>
 #include <string>
+#include <vector>
 
 #include "move.hpp"
 #include "position.hpp"
 #include "square.hpp"
+#include "undo_info.hpp"
 
 class Engine {
    public:
-    void set_position(const std::string& fen) { m_position.set_from_fen(fen); }
-
-    void do_move(Move move) { m_position.do_move(move); }
+    void setPosition(const std::string& fen) { m_position.setPosition(fen); }
 
     uint64_t perft(int depth) { return 0; /*m_position.perft(depth);*/ }
 
     [[nodiscard]] auto board() const { return m_position.board(); }
     [[nodiscard]] auto at(Square square) { return m_position.at(square); }
-    [[nodiscard]] auto moves() const { return m_position.generate_moves<GenerationType::LEGAL>(); }
-    [[nodiscard]] auto moves(Square square) const {
-        std::vector<Move> filtered;
-        auto              all = m_position.generate_moves<GenerationType::LEGAL>();
-        std::copy_if(all.begin(), all.end(), std::back_inserter(filtered),
-                     [square](const Move& m) { return m.from() == square; });
-        return filtered;
+    [[nodiscard]] auto moves() const { return m_position.generateMoves<GenerationType::LEGAL>(); }
+    [[nodiscard]] auto moves(Square square) const { return m_position.generateMoves<GenerationType::LEGAL>(square); }
+
+    void makeMove(Move move) {
+        auto moves = m_position.generateMoves<GenerationType::LEGAL>();
+        auto it =
+            std::ranges::find_if(moves, [&](const Move& m) { return m.from() == move.from() && m.to() == move.to(); });
+        if (it != moves.end()) {
+            auto undo_info = m_position.makeMove(*it);
+            m_history.push(*it, undo_info);
+        }
+    }
+    void unmakeMove() {
+        auto [move, undo_info] = m_history.pop();
+        m_position.unmakeMove(move, undo_info);
     }
 
    private:
     Position m_position;
+
+    struct HistoryEntry {
+        Move     move;
+        UndoInfo undo_info;
+    };
+
+    class History {
+       public:
+        void push(Move move, UndoInfo undo_info) { m_entries.push_back({move, undo_info}); }
+
+        HistoryEntry pop() {
+            if (m_entries.empty()) return {};
+            auto last = m_entries.back();
+            m_entries.pop_back();
+            return last;
+        }
+
+       private:
+        std::vector<HistoryEntry> m_entries;
+    };
+
+    History m_history;
 };
