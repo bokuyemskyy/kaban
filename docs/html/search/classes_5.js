b430598c08398bc7ec307c5de9e3f9a1a36ff80a
Ivan Bokuyemskyy <ivan.bokuyemskyy@gmail.com>
2025-10-13 21:24:24 +0200

    temp: remove docs

--- docs/html/search/classes_5.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['game_0',['Game',['../classGame.html',1,'']]],
-  ['glfwwindow_1',['GLFWwindow',['../classGLFWwindow.html',1,'']]],
-  ['glfwwrapper_2',['GLFWWrapper',['../classGLFWWrapper.html',1,'']]]
-];
